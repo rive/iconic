fa5fef06a9d392de7d3e3f0e0d2fb89f9f65b1b0
Guo Yunhe <i@guoyunhe.me>
2019-08-25 12:18:36 +0300

    support directory arguments

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
-const iconic = require('../lib');
+const path = require("path");
+const iconic = require("../lib");
 
-iconic(process.cwd());
+const dirs = process.argv.slice(2);
+
+if (dirs.length) {
+    dirs.forEach(d => iconic(path.join(process.cwd(), d)));
+} else {
+    iconic(process.cwd());
+}
